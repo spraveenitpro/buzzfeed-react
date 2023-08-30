098819ae6d82a1f9e0c3bd7ac0094aba0855d4aa
Praveen <spraveenitpro@gmail.com>
2023-08-30 16:18:42 -0700

    Add json to existing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react"
+import Title from "./components/Title"
+
+
+
 const App = () => {
+	const [quiz, setQuiz] = useState(false)
+	const fetchData = async () => {
+		try {
+			const response = await fetch("http://localhost:8000/quiz")
+			const json = await response.json()
+			setQuiz(json)
+			//console.log(quiz)
+		}
+		catch (err) {
+			console.log(err)
+		}
+
+
+	}
+	useEffect(() => {
+		fetchData()
+	}, [])
+	console.log(quiz)
+
 	return (
 
-			<div>
-
-				<h2>It is {new Date().toLocaleTimeString()}.</h2>
-			</div>
+		<div className="app">
+			<Title title={quiz?.title} subtitle={quiz?.subtitle} />
+		</div>
 	)
 }
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,10 @@
-const Title = () => {
-	return <div></div>
+const Title = ({ title, subtitle }) => {
+	return (
+		<div>
+			<h1>{title}</h1>
+			<p>{subtitle}</p>
+		</div>
+	)
 }
 
 export default Title
