fe2f29a8b806ba6c9ee561bb4251e6151c2da2a1
Praveen <spraveenitpro@gmail.com>
2023-08-30 15:02:59 -0700

    Setup initial scaffold

---
+++ src/components/AnswerBlock.jsx
@@ -0,0 +1,5 @@
+const AnswerBlock = () => {
+    return <div></div>
+}
+
+export default AnswerBlock
---
+++ src/components/QuestionBlock.jsx
@@ -0,0 +1,5 @@
+const QuestionBlock = () => {
+    return <div></div>
+}
+
+export default QuestionBlock
